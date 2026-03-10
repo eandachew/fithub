10ed75c998873f6c2af4fe8e2fee9a48acc15e57
eandacew.Elias <eandachew@gmail.com>
2026-03-10 17:58:23 +0000

    Workout list page display and sample added

--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import WorkoutPlan
 
 # Create your views here.
+
+def workout_list(request):
+    workouts = WorkoutPlan.objects.all()
+
+    context = {
+        'workouts': workouts
+    }
+
+    return render(request, 'workouts/workout_list.html', context)
