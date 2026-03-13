e311b578e05410f610fba607dae3f20306cbd0c9
eandacew.Elias <eandachew@gmail.com>
2026-03-13 13:25:13 +0000

    premium workout workout access added

--- workouts/views.py
+++ workouts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import WorkoutPlan
 
 # Create your views here.
@@ -15,6 +16,11 @@ def workout_list(request):
 
 def workout_detail(request, workout_id):
     workout = get_object_or_404(WorkoutPlan, id=workout_id)
+
+    # Restrict premium workouts
+    if workout.is_premium and not request.user.is_authenticated:
+        messages.warning(request, "This workout is premium. Please login or subscribe.")
+        return redirect('workouts')
 
     context = {
         'workout': workout
