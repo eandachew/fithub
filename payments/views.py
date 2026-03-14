59f8d6ba9fc4d85bf647af58f1e9d9552a94a150
eandacew.Elias <eandachew@gmail.com>
2026-03-14 16:39:06 +0000

    implement Stripe checkout session for premium subscription

--- payments/views.py
+++ payments/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 import stripe
+from django.urls import reverse
 
 # Create your views here.
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-
 def checkout(request):
 
-    context = {
-        "stripe_public_key": settings.STRIPE_PUBLIC_KEY
-    }
+    if request.method == "POST":
 
-    return render(request, "payments/checkout.html", context)
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'eur',
+                    'product_data': {
+                        'name': 'FitHub Premium Access',
+                    },
+                    'unit_amount': 1500,
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=request.build_absolute_uri(reverse('payment_success')),
+            cancel_url=request.build_absolute_uri(reverse('checkout')),
+        )
+
+        return redirect(session.url)
+
+    return render(request, "payments/checkout.html")
 
 
 def payment_success(request):
-
     return render(request, "payments/success.html")
