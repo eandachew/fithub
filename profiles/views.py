6dd04a19abb9cd90cb3fb42f1f07b21e32475862
eandacew.Elias <eandachew@gmail.com>
2026-03-15 17:24:02 +0000

    added image bio premium status added on profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from shop.models import Order
+from payments.models import UserProfile
+
 
 # Create your views here.
 
 @login_required
-def profile(request):
+def profile_home(request):
 
-    orders = Order.objects.filter(user=request.user)
+    # Get user's profile
+    profile = UserProfile.objects.get(user=request.user)
+
+    # Get user's orders
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
 
     context = {
+        "profile": profile,
         "orders": orders
     }
-
-    return render(request, "profiles/profile.html", context)
+    return render(request, "profiles/profile_home.html", context)
