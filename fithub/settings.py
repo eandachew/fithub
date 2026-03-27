452f175b6944565cd7128a48dc2fd47bcbb1d3d6
eandacew.Elias <eandachew@gmail.com>
2026-03-27 16:35:30 +0000

    env Debug added

--- fithub/settings.py
+++ fithub/settings.py
@@ -13,7 +13,6 @@ import os
 if os.path .isfile('env.py'):
     import env
 import dj_database_url
-
 from pathlib import Path
 
 
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
