5157757b24ce131fba885ace0867b274c5d3c293
eandacew.Elias <eandachew@gmail.com>
2026-04-03 15:54:13 +0100

    Update homepage design with features and products section

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from shop.models import Product  
 
 # Create your views here.
 """A view to return the index page """
 
 def index(request):
-    return render(request, "home/index.html")
+    
+    featured_products = Product.objects.all()[:2]
+    
+    context = {
+        'featured_products': featured_products
+    }
+    return render(request, "home/index.html", context)
